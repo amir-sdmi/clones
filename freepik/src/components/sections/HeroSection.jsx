852e9daf2a97c51d75ffca6a3c55e1189a00d993
amirrrrrrr7 <amir.sdmi94@gmail.com>
2024-07-29 19:49:34 +0200

    WIP06

--- freepik/src/components/sections/HeroSection.jsx
+++ freepik/src/components/sections/HeroSection.jsx
@@ -3,8 +3,8 @@ function HeroSection() {
   return (
     <div className={styles.heroSection}>
       <img src="https://fps.cdnpk.net/home/cover/image-14.webp?w=1400&amp;h=438" />
-
-      <div>
+      <div className={styles.heroOverlay} />
+      <div className={styles.heroContent}>
         <h1>Create great designs, faster</h1>
         <p>
           High-quality photos, videos, vectors, PSD, AI images, icons... to go
